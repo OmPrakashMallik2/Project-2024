7279686685247512bf2c508640488dea1959a67e
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-11-15 19:32:36 +0530

    yeahhh

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+
+  const [count, setCount] = useState(110)
+
+  const numInc = () => {
+    setCount(count+1)
+  }
+  return (
+    <div className='m-5  p-5 bg-slate-700 text-white'>
+      <h1 className='font-bold text-5xl bg-blue-300 p-2'>{count}</h1>
+      <button onClick={numInc} className='font-bold bg-green-300 rounded m-2 justify-center p-2'>Click Here</button>
+    </div>
+  )
+}
+
+export default App
