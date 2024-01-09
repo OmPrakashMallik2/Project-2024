915f30eff93279aa2c741cf2992aa361c7e559a0
Om Prakash Mallik <opm202000@gmail.com>
2024-01-09 20:57:59 +0530

    built key and partner section

--- src/component/KeyIndicators.js
+++ src/component/KeyIndicators.js
@@ -8,35 +8,35 @@ const KeyIndicators = () => {
                 <h1 className='py-2 text-5xl font-bold'>PM Vishwakarma</h1>
                 <p className='py-1 text-3xl font-normal' >Key indicators</p>
             </div>
-            <div className="flex flex-wrap gap-10">
-                <div className='bg-orange-200 p-5 w-50 h-50'>
-                    <ContentPasteIcon className='text-white bg-red-400 rounded-full' />
-                    <h2>58,81,834</h2>
-                    <p>No of Applications Submitted</p>
+            <div className="grid grid-cols-4 gap-9 pt-10">
+                <div className='bg-orange-200 p-12 rounded-lg'>
+                    <ContentPasteIcon className='' />
+                    <h2 className='text-3xl font-bold'>58,81,834</h2>
+                    <p className='text-base'>No of Applications Submitted</p>
                 </div>
-                <div className='bg-blue-200 p-5 w-50 h-50'>
+                <div className='bg-blue-200 p-12 rounded-lg'>
                     {/* logo */}
-                    <ContentPasteIcon />
-                    <h2>10,19,371</h2>
-                    <p>Stage 1 Verification Completed: Gram Panchayat or ULB Level.</p>
+                    <ContentPasteIcon className='' />
+                    <h2 className='text-3xl font-bold'>10,19,371</h2>
+                    <p className='text-base'>Stage 1 Verification Completed: Gram Panchayat or ULB Level.</p>
                 </div>
-                <div className='bg-yellow-200 p-5 w-50 h-50'>
+                <div className='bg-yellow-200 p-12 rounded-lg'>
                     {/* logo */}
-                    <ContentPasteIcon />
-                    <h2>4,31,350</h2>
-                    <p>Stage 2 Verification Completed: District Implementation Committee</p>
+                    <ContentPasteIcon className='' />
+                    <h2 className='text-3xl font-bold'>4,31,350</h2>
+                    <p className='text-base'>Stage 2 Verification Completed: District Implementation Committee</p>
                 </div>
-                <div className='bg-red-200 p-5 w-50 h-50'>
+                <div className='bg-red-200 p-12 rounded-lg'>
                     {/* logo */}
-                    <ContentPasteIcon />
-                    <h2>2,01,503</h2>
-                    <p>Stage 3 Verification Completed: Screening Committee</p>
+                    <ContentPasteIcon className='' />
+                    <h2 className='text-3xl font-bold'>2,01,503</h2>
+                    <p className='text-base'>Stage 3 Verification Completed: Screening Committee</p>
                 </div>
-                <div className='bg-green-200 p-5 w-50 h-50'>
+                <div className='bg-green-200 p-12 rounded-lg'>
                     {/* logo */}
-                    <ContentPasteIcon />
-                    <h2>1,73,417</h2>
-                    <p>No of Applicants Successfully Registered</p>
+                    <ContentPasteIcon className='' />
+                    <h2 className='text-3xl font-bold'>1,73,417</h2>
+                    <p className='text-base'>No of Applicants Successfully Registered</p>
                 </div>
             </div>
         </div>
--- src/component/Partners.js
+++ src/component/Partners.js
@@ -1,5 +1,19 @@
 import React from 'react'
-import Logo from '../media/logo12.png'
+import Logo1 from '../media/logo1.png'
+import Logo2 from '../media/logo2.png'
+import Logo3 from '../media/logo3.png'
+import Logo4 from '../media/logo4.png'
+import Logo5 from '../media/logo5.png'
+import Logo6 from '../media/logo6.png'
+import Logo7 from '../media/logo7.png'
+import Logo8 from '../media/logo8.png'
+import Logo9 from '../media/logo9.png'
+import Logo10 from '../media/logo10.png'
+import Logo11 from '../media/logo11.png'
+import Logo12 from '../media/logo12.png'
+import Logo13 from '../media/logo13.png'
+import Logo14 from '../media/logo14.png'
+import Logo15 from '../media/logo15.png'
 
 function Partners() {
   return (
@@ -7,24 +21,24 @@ function Partners() {
     <div className='p-24'>
       <div className='text-center'>
         <h1 className='py-2 pb-5 text-5xl font-bold'>Our Partners</h1>
-        <p className='pb-10 text-m font-normal'>PM Vishwakarma is a team effort! With partners like you, We can empower every Indian to achieve their dreams.</p>
+        <p className='pb-10 text-m font-normal items-center'>PM Vishwakarma is a team effort! With partners like you, We can empower every Indian to achieve their dreams.</p>
       </div>
-      <div className="px-48 pt-10 flex flex-wrap">
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
-        <img className='bg-white p-6 rounded m-3 w-40 h-auto' src={Logo} alt='partner' />
+      <div className="px-40 grid grid-cols-5 gap-5 p-10 item-center">
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo1} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo2} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo3} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo4} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo5} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo6} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo7} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo8} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo9} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo10} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo11} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo12} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo13} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo14} alt='partner' />
+        <img className='bg-white p-6 rounded object-cover w-auto h-24' src={Logo7} alt='partner' />
       </div>
     </div>
   )
